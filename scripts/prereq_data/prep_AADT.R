dcf24e96ad5e19431cb87c58dfe9d6ae501fdb52
Labibsm <31006280+labiblm@users.noreply.github.com>
2021-09-28 00:11:26 +0100

    combine addt

--- scripts/prereq_data/prep_AADT.R
+++ scripts/prereq_data/prep_AADT.R
@@ -6,7 +6,8 @@ library(dplyr)
 
 aadt_network <- st_read("../bigdata/AADT/GreaterManchester/aadt_2019.shp") #read shapefile
 #Add unique ID
-aadt_network <- aadt_network %>% mutate(UID = row_number())
+aadt_network <- aadt_network %>% mutate(UID = row_number()) %>%
+  mutate(aadt_all = c1_2019 + c2_2019 + c3_2019 + c4a_2019 + c4b_2019)
 
 #Save as RDS for other usage later
 saveRDS(aadt_network, "../bigdata/AADT/GreaterManchester/aadt_2019.Rds")
